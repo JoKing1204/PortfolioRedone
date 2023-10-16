a3b87b87128418441cee57f9bc3a4228f6c21311
JoKing1204 <jking24@basecampcodingacademy.org>
2023-10-16 11:55:59 -0500

    Im gonna make a better one

---
+++ main.js
@@ -0,0 +1,9 @@
+function startShake() {
+  const image = document.getElementById('shakeImage');
+  image.classList.add('shake');
+}
+
+function stopShake() {
+  const image = document.getElementById('shakeImage');
+  image.classList.remove('shake');
+}
