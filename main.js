28c65f0e275832f0d9a03eb8054573066b64f9a8
JoKing1204 <jking24@basecampcodingacademy.org>
2024-02-05 14:02:34 -0600

    work

--- main.js
+++
@@ -1,9 +0,0 @@
-function startShake() {
-  const image = document.getElementById('shakeImage');
-  image.classList.add('shake');
-}
-
-function stopShake() {
-  const image = document.getElementById('shakeImage');
-  image.classList.remove('shake');
-}
